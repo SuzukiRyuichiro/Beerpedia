532629073e01702545673a55724c8cc2513bdcdb
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-02-21 15:26:14 +0900

    now the hop and malt names are unique

--- app/models/hop.rb
+++ app/models/hop.rb
@@ -2,4 +2,5 @@ class Hop  < ApplicationRecord
   has_many :recipe_hops
 
   validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- app/models/malt.rb
+++ app/models/malt.rb
@@ -2,4 +2,5 @@ class Malt < ApplicationRecord
   has_many :recipe_malts
 
   validates :name, presence: true
+  validates :name, uniqueness: true
 end
