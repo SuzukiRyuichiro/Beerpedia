b3fd3fd2e5ce117f57248cd9ecc2a9a8791705c1
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-02-21 16:18:38 +0900

    generated two beer and breweries controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'breweries/index'
+  get 'breweries/create'
+  get 'breweries/new'
+  get 'breweries/show'
+  get 'beers/index'
+  get 'beers/create'
+  get 'beers/new'
+  get 'beers/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :beers, only: [:index, :create, :new, :show]
+
+  resources :breweries, only: [:index, :create, :new, :show] do
+    resources :beers, only: [:index, :create, :new, :show]
+  end
 end
