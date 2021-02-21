6605fbe76f879306f250ceb75574775d84afdbcd
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-02-21 22:20:23 +0900

    now the hops and malts can look for beers that they are used in thanks to the throughgit add .git add .git add . "

--- app/models/hop.rb
+++ app/models/hop.rb
@@ -1,5 +1,7 @@
 class Hop < ApplicationRecord
   has_many :recipe_hops
+  has_many :recipes, through: :recipe_hops
+  has_many :beers, through: :recipes
 
   validates :name, presence: true
   validates :name, uniqueness: true
--- app/models/malt.rb
+++ app/models/malt.rb
@@ -1,5 +1,7 @@
 class Malt < ApplicationRecord
   has_many :recipe_malts
+  has_many :recipes, through: :recipe_malts
+  has_many :beers, through: :recipes
 
   validates :name, presence: true
   validates :name, uniqueness: true
